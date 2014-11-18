17e1011d88435c7dee17af03d74bac581d1f8b0a
Simon Kruska <simon.kruska@googlemail.com>
2014-11-18 20:51:49 +0100

    created a test mailer

---
+++ js/mail.js
@@ -0,0 +1,35 @@
+(function() {
+
+
+'use strict';
+
+var nodemailer = require("nodemailer");
+var mail = require('../private-mail.json');
+
+var transporter = nodemailer.createTransport({ 
+   host: mail.host,
+   secuire: true,
+
+   auth: {
+       user: mail.user,
+       pass: mail.password
+   }
+});
+
+var mailOptions = {
+   from: mail.from, // sender address
+   to: mail.to, // comma separated list of receivers
+   subject: "Hello Test", // Subject line
+   text: "Hello world" // plaintext body
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+   if (error) {
+       console.log(error);
+   }
+   else {
+       console.log("Message sent: " + info.response);
+   }
+});
+
+})();
